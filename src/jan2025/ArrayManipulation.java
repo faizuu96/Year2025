3cce626a8c619e6f47ee49ca1b0c5e50c0ed9eda
faizuu96 <mohamedfaizuu@gmail.com>
2025-01-26 01:23:08 +0400

    Jan26

---
+++ src/jan2025/ArrayManipulation.java
@@ -0,0 +1,38 @@
+package jan2025;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+public class ArrayManipulation {
+    public static void main(String [] arg) {
+
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            arr.add(s.nextInt());
+        }
+
+        int numOperation = s.nextInt();
+
+        for(int i=0; i<numOperation; i++){
+            String operation = s.next();
+
+            if(operation.equals("Insert")){
+                int index = s.nextInt();
+                int value = s.nextInt();
+                arr.add(index,value);
+            }else if(operation.equals("Delete")){
+                int index = s.nextInt();
+                arr.remove(index);
+            }
+        }
+
+        for(int val : arr) {
+            System.out.print(val + " ");
+        }
+
+    }
+}
