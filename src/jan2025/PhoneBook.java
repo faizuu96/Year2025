6ec3a4dd6014c0b1fbcb209e47057a5720b13795
faizuu96 <mohamedfaizuu@gmail.com>
2025-01-27 02:38:01 +0400

    Jan26

---
+++ src/jan2025/PhoneBook.java
@@ -0,0 +1,34 @@
+package jan2025;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PhoneBook{
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        s.nextLine();
+
+        HashMap<String, String> phoneBook = new HashMap<>();
+
+        for(int i=0; i<n; i++) {
+            String name = s.nextLine();
+            String phoneNumber = s.nextLine();
+            phoneBook.put(name, phoneNumber);
+        }
+
+        while(s.hasNextLine()) {
+            String query = s.nextLine();
+
+            if(phoneBook.containsKey(query)) {
+                System.out.println(query + "=" + phoneBook.get(query));
+            }else {
+                System.out.println("Not found");
+            }
+        }
+
+         s.close();
+
+    }
+}
