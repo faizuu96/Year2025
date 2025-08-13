545da3cd4eb274ea2939a149efc883188ca68356
faizuu96 <mohamedfaizuu@gmail.com>
2025-08-13 23:17:20 +0400

    Aug 13 2025

---
+++ src/aug2025/BeginnerVariable.java
@@ -0,0 +1,18 @@
+package aug2025;
+
+public class BeginnerVariable {
+        public static void main(String[] args) {
+            // 1. Declare variables
+            String name = "Rahul";
+            int age = 20;
+            double height = 5.9;
+            boolean isProgrammer = true;
+
+            // 2. Print them
+            System.out.println("Name: " + name);
+            System.out.println("Age: " + age);
+            System.out.println("Height: " + height + " feet");
+            System.out.println("Is a Programmer? " + isProgrammer);
+        }
+    }
+
